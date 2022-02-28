625f362c0e2bff86e087e99030d6a75bdb7d3aeb
FreddyJS <sebashgt@gmail.com>
2022-02-28 19:05:45 +0100

    [backend] Using postgreSQL with ArrayFields
    Route field: Now its an array of string with max length 10 chars

--- backend/routes/models.py
+++ backend/routes/models.py
@@ -1,6 +1,7 @@
+from django.contrib.postgres.fields import ArrayField
 from django.db import models
 
 # Create your models here.
 class Route(models.Model):
   room = models.CharField(max_length=5, unique=True)
-  route = models.TextField()
+  route = ArrayField(models.CharField(max_length=10))
--- backend/routes/serializers.py
+++ backend/routes/serializers.py
@@ -5,14 +5,4 @@ from .models import Route
 class RouteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Route
-        fields = ['room']
-    
-    def to_representation(self, instance):
-        rep =  super().to_representation(instance)
-        rep['route'] = []
-
-        steps = instance.route.split(';')
-        for step in steps:
-            rep['route'].append(step)
-
-        return rep
+        fields = ['room', 'route']
