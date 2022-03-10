1f4e3c8275c0f6fa4e134bb521b80fabedb1e4f1
FreddyJS <sebashgt@gmail.com>
2022-03-10 22:34:06 +0100

    [frontend] Style changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,37 +5,22 @@ import './App.scss';
 import PageHeader from './components/PageHeader';
 
 import { Route, Switch } from 'react-router-dom';
-import { Content, Button } from 'carbon-components-react';
+import { Content } from 'carbon-components-react';
 
 import RoomPage from './content/RoomPage';
-import { RobotSocket, UISocket } from './sockets';
 
 import Dashboard from './content/Dashboard';
 import DashboardDev from './content/Dashboard/DashboardDev';
 
 function App() {
-  const sendHelllo = (from) => {
-    const data = {
-      type: from === 'UI' ? 'to.robot' : 'to.ui',
-      message: "Hello from " + from,
-    };
-
-    if (from === "UI") {
-      UISocket.send(JSON.stringify(data));
-    } else {
-      RobotSocket.send(JSON.stringify(data));
-    }
-  };
-
   return (
     <>
       <PageHeader/>
       <Content>
-        <div style={{display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center"}}>
-          <Button style={{margin: "0.1rem"}} onClick={() => sendHelllo("UI")}> Client Message </Button>
-          <Button style={{margin: "0.1rem"}} onClick={() => sendHelllo("Robot")}> Robot Message </Button>    
+        <div className="room-page__header">
+          <h1>guiaMe: Automated Guiding Robot</h1>
+          <p>Un guía de confianza para gente de todo tipo</p>
         </div>
-
         <Switch>
           <Route exact path="/" component={Dashboard} />
           <Route exact path="/mapdev" component={DashboardDev} />
@@ -44,7 +29,6 @@ function App() {
       </Content>
     </>
   );
-
 }
 
 export default App;
