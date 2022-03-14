d79762cd59addc2a4127fd3762d266b04e7e61b6
FreddyJS <sebashgt@gmail.com>
2022-03-14 12:27:50 +0100

    [frontend] Fixed error

--- frontend/src/content/Dashboard/Dashboard.js
+++ frontend/src/content/Dashboard/Dashboard.js
@@ -5,36 +5,37 @@ import { Stage, Layer, Rect, Text } from 'react-konva';
 
 const Dashboard = () => {
   const stageRef = React.useRef(null);
+  const rectRef = React.useRef(null);
   const hall = 0;
 
-  const rectRef = React.useRef(null);
-  const [blink, setBlink] = React.useState(false);
   React.useEffect(() => {
-    console.log(rectRef);
-    if (!rectRef.current) {
-      return;
+    if (rectRef.current !== null) {
+      var period = 300;
+  
+      var anim = new Konva.Animation(frame => {
+        if (rectRef.current !== null) {
+          rectRef.current.opacity((Math.sin(frame.time / period) + 1) / 2);
+        }
+          
+      }, rectRef.current.getLayer());
+  
+      anim.start();
+      return () => {
+        anim.stop();
+      };
     }
-    var period = 300;
-
-    var anim = new Konva.Animation(frame => {
-      rectRef.current.opacity((Math.sin(frame.time / period) + 1) / 2);
-    }, rectRef.current.getLayer());
-
-    anim.start();
-    return () => {
-      anim.stop();
-    };
-  }, [blink]);
+  }, []);
 
   return (
-    
     <Stage width={stage.stageWidth} height={stage.stageHeight} ref={stageRef}>
       {stage.layers.map((layer, index) => {
+        const hallId = "hall" + hall;
+
         return (
           <Layer key={index}>
             {layer.elements.map((element, index) => {
               if (element.type === "Rect") {
-                return <Rect key={index} {...element.attrs} fill={element.attrs.id === "hall" + hall ? "orange" : element.attrs.fill } ref={element.attrs.id === "hall" + hall ? rectRef : null} />; 
+                return <Rect key={index} {...element.attrs} fill={element.attrs.id === hallId ? "orange" : element.attrs.fill } ref={element.attrs.id === hallId ? rectRef : null} />; 
               } else if (element.type === "Text") {
                 return <Text key={index} {...element.attrs} />
               }
