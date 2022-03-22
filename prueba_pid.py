50af83649fb99edf4a17063e77ce00e59477b2cb
pablofp7 <pablofernandezpi@gmail.com>
2022-03-22 13:58:34 +0100

    socket_pid

--- prueba_pid.py
+++ prueba_pid.py
@@ -1,12 +1,11 @@
 from simple_pid import PID
 import random
 import time
-
-pid = PID(1, 0.1, 0.05, setpoint=50)
-pid.output_limits = (-100, 100)
-pid.sample_time = (0.1) # por defecto 0.01
+import socket
 
 
+
+'''
 
 randomlist = []
 for i in range(0,10):
@@ -23,4 +22,47 @@ print(randomlist)
 for element in randomlist:
     control = pid(element)
     print (control)
-    time.sleep(0.25)
+    time.sleep(0.25)	
+
+'''
+
+# next create a socket object
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.settimeout(2)
+
+
+# reserve a port on your computer in our
+# case it is 12345 but it can be anything
+port = 12345			
+
+server_address = ('0.0.0.0', port)
+sock.bind(server_address)
+sock.setblocking(1)
+print('starting up on {} port {}'.format(*server_address))
+	
+data, address = sock.recvfrom(1500)
+
+velocidad_objetivo = data[:-1].decode()
+print("Velocidad objetivo = " + str(velocidad_objetivo))
+
+pid = PID(1, 0.1, 0.05, setpoint=int(velocidad_objetivo))
+pid.output_limits = (-100, 100)
+pid.sample_time = (0.1) # por defecto 0.01
+lista = []
+
+while True:
+
+    data, address = sock.recvfrom(1500)
+    data = data [:-1]
+    print("Dato recibido:" + str(data.decode()))
+    if("Hello" in data.decode()):
+        print("Fin de la transmisión.")
+        break
+    dato = int(data.decode())
+    
+    control = pid(dato)
+    lista.append(control)
+    #time.sleep(0.25)
+
+print(lista)
+
