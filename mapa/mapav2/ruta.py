b05eb6d8c0c829359281ac990b5d3ea808056520
FreddyJS <sebashgt@gmail.com>
2022-04-03 17:08:03 +0200

    [mapa] Minimal changes

--- mapa/mapav2/ruta.py
+++ mapa/mapav2/ruta.py
@@ -4,7 +4,6 @@ import sys
 import json
 import os
 
-from sqlalchemy import false
 
 class Nodo:
     def __init__(self, id, padre, coste_acumulado):
@@ -27,7 +26,7 @@ class ListaNodos:
             if (n.coste_acumulado < coste):
                 resultado = n
                 coste = n.coste_acumulado
-        
+
         self.nodos.remove(resultado)
         return resultado
 
@@ -35,7 +34,7 @@ class ListaNodos:
         for n in self.nodos:
             if (n.id == id):
                 return n.padre
-        
+
         return None
 
     def vacia(self):
@@ -44,30 +43,27 @@ class ListaNodos:
         else:
             return False
 
-
     def contiene(self, nodo):
         for n in self.nodos:
             if (n.id == nodo.id):
                 return True
-        
+
         return False
 
     def imprime(self):
         print("------------- FRONTERA -----------")
         for n in self.nodos:
-            print("MARCA: " + n.id + ", COSTE ACUMULADO: " + str(n.coste_acumulado))
-
-
+            print("MARCA: " + n.id + ", COSTE ACUMULADO: " +
+                  str(n.coste_acumulado))
 
 
 class Mapa:
-    def __init__(self,fichero):
+    def __init__(self, fichero):
         with open(fichero, 'r') as f:
-            data=f.read()
+            data = f.read()
             f.close()
 
         self.mapa = json.loads(data)
-
 
     def enlaces(self, id):
         for marca in self.mapa:
@@ -75,7 +71,7 @@ class Mapa:
                 return marca["enlaces"]
 
         return []
-    
+
     def esCruce(self, id):
         for marca in self.mapa:
             if (marca["id"] == id):
@@ -91,7 +87,7 @@ class Mapa:
 
     def getMarca(self, habitacion_id):
         for marca in self.mapa:
-            if (marca["tipo"] == "habitaciones"):    
+            if (marca["tipo"] == "habitaciones"):
                 if (habitacion_id in marca["habitaciones"]):
                     return marca["id"]
                 if(habitacion_id in marca["id"]):
@@ -102,9 +98,7 @@ class Mapa:
                 if(habitacion_id in marca["id"]):
                     return marca["id"]
 
-    
         return None
-
 
 
 def get_ruta(mapa, inicio, fin):
@@ -120,8 +114,7 @@ def get_ruta(mapa, inicio, fin):
         print("Número introducido no válido.")
         exit(-1)
 
-
-    nodo = Nodo(marca_inicio,marca_inicio,0)
+    nodo = Nodo(marca_inicio, marca_inicio, 0)
     frontera.push(nodo)
 
     while (not frontera.vacia()):
@@ -132,8 +125,8 @@ def get_ruta(mapa, inicio, fin):
             paso = nodo.padre
             while (paso != marca_inicio):
                 paso = visitados.getPadre(paso)
-                resultado.insert(0,paso)
-        
+                resultado.insert(0, paso)
+
             return resultado
 
         visitados.push(nodo)
@@ -146,7 +139,7 @@ def get_ruta(mapa, inicio, fin):
                 frontera.push(nodo_hijo)
 
 
-def color(mapa,ruta):
+def color(mapa, ruta):
 
     aux = []
     for elemento in ruta:
@@ -154,8 +147,9 @@ def color(mapa,ruta):
             aux.append("_CRUCE.")
         else:
             aux.append("_HABITACION.")
-    
+
     return (aux)
+
 
 def get_direcciones(mapa, ruta):
 
@@ -169,9 +163,9 @@ def get_direcciones(mapa, ruta):
                     if(mapa.getNodo(ruta[i+1])["tipo"] in "cruce" or mapa.getNodo(ruta[i+1])["tipo"] in "habitaciones"):
                        # print(ruta[i+1] + " es un: " + mapa.getNodo(ruta[i+1])["tipo"] +  " - ", end="")
                         direcciones.append(enl.get("direccion"))
-        
-            i+=1
-    except IndexError as error:  
+
+            i += 1
+    except IndexError as error:
         #se añade ultimo enlace, no se añadió antes puesto que para el utlimo enlace lee que el siguiente nodo es de habnitaciones ->
         #no entra if, no lo añade
         n = len(ruta)-2
@@ -186,10 +180,12 @@ def get_direcciones(mapa, ruta):
         for elemento in ruta:
             if (mapa.esCruce(elemento)):
                 ult_cruce = ctr
-            ctr +=1
+            ctr += 1
 
-        direcciones[ult_cruce - 1] = str(direcciones[ult_cruce - 1]) + "-Este es el último cruce." #se le añade esta string a la direccion para saber que es el ultimo cruce
-        direcciones.pop() #se quita el ultimo elemento porque se repite
+        # se le añade esta string a la direccion para saber que es el ultimo cruce
+        direcciones[ult_cruce -
+                    1] = str(direcciones[ult_cruce - 1]) + "-Este es el último cruce."
+        direcciones.pop()  # se quita el ultimo elemento porque se repite
     return direcciones
 
 
@@ -199,8 +195,9 @@ if __name__ == "__main__":
         print("\nSintaxis: python3 ruta.py nombre_mapa.json origen destino direccion_actual(grados-> norte 0, este 90...)\n")
         exit(-1)
 
-    if (os.path.exists("./rutas/" + sys.argv[2] + "_" +  sys.argv[3] + ".txt")):
-        print("Ruta de " + sys.argv[2] + " a " + sys.argv[3] + " ya calculada anteriormente.\n")
+    if (os.path.exists("./rutas/" + sys.argv[2] + "_" + sys.argv[3] + ".txt")):
+        print("Ruta de " + sys.argv[2] + " a " +
+              sys.argv[3] + " ya calculada anteriormente.\n")
         exit(-2)
 
     mapa = Mapa(sys.argv[1])
@@ -214,8 +211,6 @@ if __name__ == "__main__":
     giros = []
     aux = ""
 
-
-
     for d in ruta_absoluta:
        grados = grados % 360
 
@@ -223,28 +218,28 @@ if __name__ == "__main__":
            if ("norte" in d):
                grados += 0
                if("cruce" not in d):
-                   giros.append("Recto.")    
+                   giros.append("Recto.")
                else:
                    giros.append("Recto. Llegando al último cruce")
 
            elif ("oeste" in d):
                grados += 270
                if("cruce" not in d):
-                   giros.append("Izquierda.")    
+                   giros.append("Izquierda.")
                else:
                    giros.append("Izquierda. Llegando al último cruce")
-           
+
            elif ("este" in d):
                grados += 90
                if("cruce" not in d):
-                   giros.append("Derecha.")    
+                   giros.append("Derecha.")
                else:
                    giros.append("Derecha. Llegando al último cruce")
 
            elif ("sur" in d):
                grados += 180
                if("cruce" not in d):
-                   giros.append("Dar vuelta.")    
+                   giros.append("Dar vuelta.")
                else:
                    giros.append("Dar vuelta. Llegando al último cruce")
 
@@ -252,86 +247,86 @@ if __name__ == "__main__":
            if ("norte" in d):
                grados += 270
                if("cruce" not in d):
-                   giros.append("Izquierda.")    
+                   giros.append("Izquierda.")
                else:
                    giros.append("Izquierda. Llegando al último cruce")
 
            elif ("oeste" in d):
                grados += 180
                if("cruce" not in d):
-                   giros.append("Dar la vuelta.")    
+                   giros.append("Dar la vuelta.")
                else:
                    giros.append("Dar la vuelta. Llegando al último cruce")
-           
+
            elif ("este" in d):
                grados += 0
                if("cruce" not in d):
-                   giros.append("Recto.")    
+                   giros.append("Recto.")
                else:
                    giros.append("Recto. Llegando al último cruce")
 
            elif ("sur" in d):
                grados += 90
                if("cruce" not in d):
-                   giros.append("Derecha.")    
+                   giros.append("Derecha.")
                else:
                    giros.append("Derecha. Llegando al último cruce")
-    
+
        elif (grados == 270):
            if ("norte" in d):
                grados += 90
                if("cruce" not in d):
-                   giros.append("Derecha.")    
+                   giros.append("Derecha.")
                else:
                    giros.append("Derecha. Llegando al último cruce")
 
            elif ("oeste" in d):
                grados += 0
                if("cruce" not in d):
-                   giros.append("Recto.")    
+                   giros.append("Recto.")
                else:
                    giros.append("Recto. Llegando al último cruce")
-           
+
            elif ("este" in d):
                grados += 180
                if("cruce" not in d):
-                   giros.append("Dar la vuelta.")    
+                   giros.append("Dar la vuelta.")
                else:
                    giros.append("Dar la vuelta. Llegando al último cruce")
 
            elif ("sur" in d):
                grados += 270
                if("cruce" not in d):
-                   giros.append("Izquierda.")    
+                   giros.append("Izquierda.")
                else:
                    giros.append("Izquierda. Llegando al último cruce")
-        
+
        elif (grados == 180):
            if ("norte" in d):
                grados += 180
                if("cruce" not in d):
-                   giros.append("Dar la vuelta.")    
+                   giros.append("Dar la vuelta.")
                else:
                    giros.append("Dar la vuelta. Llegando al último cruce")
 
            elif ("oeste" in d):
                grados += 90
                if("cruce" not in d):
-                   giros.append("Derecha.")    
+                   giros.append("Derecha.")
                else:
                    giros.append("Derecha. Llegando al último cruce")
-           
+
            elif ("este" in d):
                grados += 270
                if("cruce" not in d):
-                   giros.append("Izquierda.")    
+                   giros.append("Izquierda.")
                else:
                    giros.append("Izquierda. Llegando al último cruce")
 
            elif ("sur" in d):
                grados += 0
                if("cruce" not in d):
-                   giros.append("Recto.")    
+                   giros.append("Recto.")
                else:
                    giros.append("Recto. Llegando al último cruce")
 
@@ -339,22 +334,24 @@ if __name__ == "__main__":
 
     counter = 0
 
-    if(giros[len(giros)-1] >=360):
+    if(giros[len(giros)-1] >= 360):
         giros[len(giros)-1] = giros[len(giros)-1] % 360
-   
+
     counter = 0
     giros_final = []
     try:
         while True:
             giros_final.append(str(giros[counter]) + str(colores[counter+1]))
-            counter +=  1 
+            counter += 1
     finally:
         giros_final.append(giros.pop())
         try:
-            outputFile = open ("./rutas/" + sys.argv[2] + "_" +  sys.argv[3] + ".txt", "w")
+            outputFile = open(
+                "./rutas/" + sys.argv[2] + "_" + sys.argv[3] + ".txt", "w")
         except:
             os.system("mkdir rutas")
-            outputFile = open ("./rutas/" + sys.argv[2] + "_" +  sys.argv[3] + ".txt", "w")
+            outputFile = open(
+                "./rutas/" + sys.argv[2] + "_" + sys.argv[3] + ".txt", "w")
         finally:
             for cruce in giros_final:
                 outputFile.write(str(cruce) + "\n")
