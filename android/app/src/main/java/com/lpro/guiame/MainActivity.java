32fe092ed1921b8c390e58784ede8df73b87dda8
FreddyJS <sebashgt@gmail.com>
2022-04-21 01:11:37 +0200

    [android] Immersive mode

--- android/app/src/main/java/com/lpro/guiame/MainActivity.java
+++ android/app/src/main/java/com/lpro/guiame/MainActivity.java
@@ -7,6 +7,7 @@ import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 
@@ -20,10 +21,25 @@ public class MainActivity extends AppCompatActivity {
     public static int BLUETOOTH_NOTIFICATION_ID = 1;
     private BluetoothAdvertiser advertiser;
 
+    private void hideSystemUI() {
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE
+                        // Set the content to appear under the system bars so that the
+                        // content doesn't resize when the system bars hide and show.
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        // Hide the nav bar and status bar
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.hideSystemUI();
 
         // WebView Setup
         WebView webView = findViewById(R.id.webview);
